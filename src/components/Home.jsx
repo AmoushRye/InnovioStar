fe98180f3731bdc2aadc8e0e6f9b4b93ff94adca
AmoushRye <amoskhams1234@gmail.com>
2023-10-20 17:03:30 +0530

    homepage complete

---
+++ src/components/Home.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import vg from "../assets/2.webp"
+import {AiFillGoogleCircle,AiFillAmazonCircle,AiFillYoutube,AiFillInstagram} from "react-icons/ai"
+const Home = () => {
+    return (
+        <>
+            <div className="home">
+                <main>
+                    <h1>TechyStar</h1>
+                    <p>Solution to all your problems</p>
+                </main>
+            </div>
+            <div className="home2">
+                <img src={vg} alt="Graphics" />
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi tempore veritatis, explicabo quidem impedit doloremque,
+                        sequi dolore nisi soluta expedita omnis. Dolores in soluta repellat exercitationem aliquam perspiciatis, hic similique nesciunt. Inventore, ex nam!</p>
+                </div>
+            </div>
+            <div className="home3">
+                <div>
+                    <h1>Who we are?</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, perferendis. Aliquam debitis quidem veniam architecto rerum, saepe odio voluptatum optio accusantium laborum aut dolores reprehenderit non aperiam ratione maxime illo at minus illum incidunt, quam consequuntur quia? Quis doloremque magnam perferendis alias velit est explicabo itaque fugiat numquam impedit possimus laborum perspiciatis pariatur deleniti, rem vero provident odit tenetur quo?</p>
+                </div>
+            </div>
+            <div className="home4">
+                <div>
+                    <h1>Brands</h1>
+                    <article>
+                        <div style ={{
+                            animationDelay:"0.3s",
+                        }}>
+                            <AiFillGoogleCircle/>
+                            <p>Google</p>
+
+                        </div>
+                        <div style ={{
+                            animationDelay:"0.5s",
+                        }}>
+                            <AiFillAmazonCircle/>
+                            <p>Amazon</p>
+
+                        </div>
+                        <div style ={{
+                            animationDelay:"0.7s",
+                        }}>
+                            <AiFillYoutube/>
+                            <p>YouTube</p>
+
+                        </div>
+                        <div style ={{
+                            animationDelay:"1s",
+                        }}>
+                            <AiFillInstagram/>
+                            <p>Instagram</p>
+
+                        </div>
+                    </article>
+                </div>
+            </div>
+
+        </>
+    );
+};
+
+export default Home
